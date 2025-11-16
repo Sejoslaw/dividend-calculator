11d376dcf00dbc36f0db574c3380aaacbe19754b
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2025-11-16 13:33:54 +0000

    !data;

--- scripts/data.js
+++ scripts/data.js
@@ -2,15 +2,15 @@
 
 const stockData = {
     "2025-11-16": [
-        { nazwa: "WIG-PL-COMPANY-A", portfel: "ING Makler", cenaZakupu: 120.50, ilosc: 10, dywidendaRoczna: 5.20 },
-        { nazwa: "WIG-PL-COMPANY-B", portfel: "XTB", cenaZakupu: 45.00, ilosc: 50, dywidendaRoczna: 1.50 }
-    ],
-    "2025-10-30": [
-        { nazwa: "WIG-PL-COMPANY-A", portfel: "ING Makler", cenaZakupu: 120.50, ilosc: 10, dywidendaRoczna: 5.20 },
-        { nazwa: "WIG-PL-COMPANY-B", portfel: "XTB", cenaZakupu: 40.00, ilosc: 40, dywidendaRoczna: 1.50 } // Mniej akcji/inna cena
-    ],
-    "2025-09-01": [
-        { nazwa: "WIG-PL-COMPANY-C", portfel: "XTB", cenaZakupu: 200.00, ilosc: 5, dywidendaRoczna: 10.00 }
+        { nazwa: "DOMDEV", portfel: "Makler 1", cenaZakupu: 84.09, ilosc: 150, dywidendaRoczna: 6.50 },
+        { nazwa: "HANDLOWY", portfel: "Makler 1", cenaZakupu: 57.76, ilosc: 2119, dywidendaRoczna: 13.73 },
+        { nazwa: "PKNORLEN", portfel: "Makler 1", cenaZakupu: 63.52, ilosc: 5809, dywidendaRoczna: 6 },
+        { nazwa: "PZU", portfel: "Makler 1", cenaZakupu: 43.98, ilosc: 1685, dywidendaRoczna: 4.47 },
+        { nazwa: "HANDLOWY", portfel: "Makler 2", cenaZakupu: 55.8756, ilosc: 587, dywidendaRoczna: 13.73 },
+        { nazwa: "PKNORLEN", portfel: "Makler 2", cenaZakupu: 64.1375, ilosc: 1277, dywidendaRoczna: 6 },
+        { nazwa: "PZU", portfel: "Makler 2", cenaZakupu: 61.98, ilosc: 200, dywidendaRoczna: 4.47 },
+        { nazwa: "PZU", portfel: "XTB 1", cenaZakupu: 62.02, ilosc: 646, dywidendaRoczna: 4.47 },
+        { nazwa: "PZU", portfel: "XTB 2", cenaZakupu: 61.98, ilosc: 103, dywidendaRoczna: 4.47 },
     ]
 };
 
